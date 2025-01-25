034dba8cf8c0bd67ce7d8b5a8c99a207b87567eb
Pawan Singhania <pawansinghania001@gmail.com>
2025-01-25 12:57:34 +0530

    change app and index for listening

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import dotenv from "dotenv";
-import express from 'express';
 import ConnectDB from "./db/index.js";
+import app from './app.js'
 
 dotenv.config();
 
-ConnectDB();
-const app = express();
-const port  = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Example app listening on port`)
-});
+ConnectDB().then(()=> {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    });
+    
+}).catch((error)=> {
+    console.log('App Listening Error:', error)
+})
 
